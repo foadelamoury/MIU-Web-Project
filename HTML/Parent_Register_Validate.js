413dce6d786bdfc2ed1fa14bf9312fc2eb456455
Ahmed ElKerdany <akerdany@gmail.com>
2018-11-18 17:12:01 +0200

    Correction

---
+++ HTML/Parent_Register_Validate.js
@@ -0,0 +1,11 @@
+function validateParentFormRegister() {
+      var x = document.forms["parentFormRegister"]["ChildFirstName"].value;
+      if (x == "" || x == null) {
+          alert("Name must be filled out");
+          return false;
+        }
+
+        else{
+          return true;
+        }
+}
